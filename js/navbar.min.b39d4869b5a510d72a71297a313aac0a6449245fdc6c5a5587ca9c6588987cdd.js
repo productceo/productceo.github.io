bb30612cd02834521ce7a4f965b04cf7bd83266b
Junwon Park <junwon@product.ceo>
2020-12-14 20:27:38 -0800

    rebuilding site Mon Dec 14 20:27:38 PST 2020

---
+++ js/navbar.min.b39d4869b5a510d72a71297a313aac0a6449245fdc6c5a5587ca9c6588987cdd.js
@@ -0,0 +1,2 @@
+if(window.innerWidth<=960){var prevScrollPos=window.pageYOffset;window.onscroll=function(){var currentScrollPos=window.pageYOffset;if(prevScrollPos>currentScrollPos){document.getElementById("navbar").style.top="0";document.getElementById("navMenu").style.top="0";}else{document.getElementById("navbar").style.top="-50vh";document.getElementById("navMenu").style.top="50vh";}
+prevScrollPos=currentScrollPos;}}
